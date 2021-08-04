ecc2b4f43d2533ba8c1391d977c681efefa723fe
Himanshu <49796034+HimanshuKGP007@users.noreply.github.com>
2021-08-04 15:39:03 +0530

    Edits

---
+++ model/finalized_model.py
@@ -0,0 +1,8 @@
+# import pickle
+# # model = emodel
+# # filename = 'finalized_model.sav'
+# # pickle.dump(model, open(filename, 'wb'))
+# # load the model from disk
+# loaded_model = pickle.load(open('C:\Users\DELL\COV_Project\models\finalized_model.sav', 'rb'))
+# result = loaded_model.score(X_test, y_test)
+# print(result)
